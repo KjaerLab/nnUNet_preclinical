114a17a90b093698860ae06084a0e9a78e4b5c9d
theasger <s204306@dtu.dk>
2026-02-11 09:13:52 +0100

    Add batched speedy predictor

--- nnunetv2/inference/CustomPredictor.py
+++ nnunetv2/inference/CustomPredictor.py
@@ -39,7 +39,8 @@ class SpeedyPredictor(nnUNetPredictor):
                  device: torch.device = torch.device('cuda'),
                  verbose: bool = False,
                  verbose_preprocessing: bool = False,
-                 allow_tqdm: bool = True):
+                 allow_tqdm: bool = True, 
+                 only_zy_flips: bool = True):
         
         super().__init__(tile_step_size,
                  use_gaussian,
@@ -50,6 +51,7 @@ class SpeedyPredictor(nnUNetPredictor):
                  verbose_preprocessing,
                  allow_tqdm)
 
+        self.only_zy_flips = only_zy_flips
         self.gaussian = None  # add new varible for only computing gaussian once
 
     def initialize_from_trained_model_folder(self, model_training_output_dir: str,
@@ -113,8 +115,11 @@ class SpeedyPredictor(nnUNetPredictor):
         self.trainer_name = trainer_name
 
         ############################## Change allowed mirroring ###############################
-        # self.allowed_mirroring_axes = inference_allowed_mirroring_axes 
-        self.allowed_mirroring_axes = [0,1]
+        if self.only_zy_flips:
+            self.allowed_mirroring_axes = [0,1]
+        else:
+            self.allowed_mirroring_axes = inference_allowed_mirroring_axes 
+        #######################################################################################
 
 
         self.label_manager = plans_manager.get_label_manager(dataset_json)
@@ -156,12 +161,14 @@ class SpeedyPredictor(nnUNetPredictor):
                                            device=results_device)
             n_predictions = torch.zeros(data.shape[1:], dtype=torch.half, device=results_device)
 
+            ###################################### Save gaussian to class variable #####################################
             if self.use_gaussian and not self.gaussian:
                 self.gaussian = compute_gaussian(tuple(self.configuration_manager.patch_size), sigma_scale=1. / 8,
                                             value_scaling_factor=10,
                                             device=results_device)
             else:
                 self.gaussian = 1
+            ############################################################################################################
 
             if not self.allow_tqdm and self.verbose:
                 print(f'running prediction: {len(slicers)} steps')
@@ -192,6 +199,192 @@ class SpeedyPredictor(nnUNetPredictor):
                                    'predicted_logits to fp32')
         except Exception as e:
             del predicted_logits, n_predictions, prediction, gaussian, workon
+            empty_cache(self.device)
+            empty_cache(results_device)
+            raise e
+        return predicted_logits
+
+
+class BatchedSpeedyPredictor(nnUNetPredictor):
+
+    def __init__(self,
+                 tile_step_size: float = 0.5,
+                 use_gaussian: bool = True,
+                 use_mirroring: bool = True,
+                 perform_everything_on_device: bool = True,
+                 device: torch.device = torch.device('cuda'),
+                 verbose: bool = False,
+                 verbose_preprocessing: bool = False,
+                 allow_tqdm: bool = True, 
+                 only_zy_flips: bool = True,
+                 batch_size: int = 4):
+        
+        super().__init__(tile_step_size,
+                 use_gaussian,
+                 use_mirroring,
+                 perform_everything_on_device,
+                 device,
+                 verbose,
+                 verbose_preprocessing,
+                 allow_tqdm)
+
+        self.only_zy_flips = only_zy_flips
+        self.gaussian = None  # add new varible for only computing gaussian once
+        self.batch_size = batch_size
+
+    def initialize_from_trained_model_folder(self, model_training_output_dir: str,
+                                             use_folds: Union[Tuple[Union[int, str]], None],
+                                             checkpoint_name: str = 'checkpoint_final.pth'):
+        """
+        This is used when making predictions with a trained model
+        """
+        if use_folds is None:
+            use_folds = nnUNetPredictor.auto_detect_available_folds(model_training_output_dir, checkpoint_name)
+
+        dataset_json = load_json(join(model_training_output_dir, 'dataset.json'))
+        plans = load_json(join(model_training_output_dir, 'plans.json'))
+        plans_manager = PlansManager(plans)
+
+        if isinstance(use_folds, str):
+            use_folds = [use_folds]
+
+        parameters = []
+        for i, f in enumerate(use_folds):
+            f = int(f) if f != 'all' else f
+            checkpoint = torch.load(join(model_training_output_dir, f'fold_{f}', checkpoint_name),
+                                    map_location=torch.device('cpu'), weights_only=False)
+            if i == 0:
+                trainer_name = checkpoint['trainer_name']
+                configuration_name = checkpoint['init_args']['configuration']
+                inference_allowed_mirroring_axes = checkpoint['inference_allowed_mirroring_axes'] if \
+                    'inference_allowed_mirroring_axes' in checkpoint.keys() else None
+
+            parameters.append(checkpoint['network_weights'])
+
+        configuration_manager = plans_manager.get_configuration(configuration_name)
+        # restore network
+        num_input_channels = determine_num_input_channels(plans_manager, configuration_manager, dataset_json)
+        trainer_class = recursive_find_python_class(join(nnunetv2.__path__[0], "training", "nnUNetTrainer"),
+                                                    trainer_name, 'nnunetv2.training.nnUNetTrainer')
+        if trainer_class is None:
+            print(f'WARNING: Unable to locate trainer class {trainer_name} in nnunetv2.training.nnUNetTrainer. '
+                               f'Please place it there (in any .py file)!\nDefault to nnUNetTrainer')
+            trainer_name = "nnUNetTrainer"
+            
+        network = trainer_class.build_network_architecture(
+            configuration_manager.network_arch_class_name,
+            configuration_manager.network_arch_init_kwargs,
+            configuration_manager.network_arch_init_kwargs_req_import,
+            num_input_channels,
+            plans_manager.get_label_manager(dataset_json).num_segmentation_heads,
+            enable_deep_supervision=False
+        )
+
+        self.plans_manager = plans_manager
+        self.configuration_manager = configuration_manager
+        self.list_of_parameters = parameters
+
+        # initialize network with first set of parameters, also see https://github.com/MIC-DKFZ/nnUNet/issues/2520
+        network.load_state_dict(parameters[0])
+
+        self.network = network
+
+        self.dataset_json = dataset_json
+        self.trainer_name = trainer_name
+
+        ############################## Change allowed mirroring ###############################
+        if self.only_zy_flips:
+            self.allowed_mirroring_axes = [0,1]
+        else:
+            self.allowed_mirroring_axes = inference_allowed_mirroring_axes 
+        #######################################################################################
+
+
+        self.label_manager = plans_manager.get_label_manager(dataset_json)
+        if ('nnUNet_compile' in os.environ.keys()) and (os.environ['nnUNet_compile'].lower() in ('true', '1', 't')) \
+                and not isinstance(self.network, OptimizedModule):
+            print('Using torch.compile')
+            self.network = torch.compile(self.network)
+
+    @torch.inference_mode()
+    def _internal_predict_sliding_window_return_logits(self,
+                                                       data: torch.Tensor,
+                                                       slicers,
+                                                       do_on_device: bool = True,
+                                                       ):
+        predicted_logits = n_predictions = prediction = gaussian = batch = None
+        results_device = self.device if do_on_device else torch.device('cpu')
+
+        try:
+            empty_cache(self.device)
+
+            # move image to device
+            if self.verbose:
+                print(f'move image to device {results_device}')
+            data = data.to(results_device)
+
+            # preallocate results arrays
+            if self.verbose:
+                print(f'preallocating results arrays on device {results_device}')
+            predicted_logits = torch.zeros((self.label_manager.num_segmentation_heads, *data.shape[1:]),
+                                           dtype=torch.half,
+                                           device=results_device)
+            n_predictions = torch.zeros(data.shape[1:], dtype=torch.half, device=results_device)
+
+            ###################################### Save gaussian to class variable #####################################
+            if self.use_gaussian and not self.gaussian:
+                self.gaussian = compute_gaussian(tuple(self.configuration_manager.patch_size), sigma_scale=1. / 8,
+                                                 value_scaling_factor=10,
+                                                 device=results_device)
+            elif not self.use_gaussian:
+                self.gaussian = 1
+            ############################################################################################################
+
+            if not self.allow_tqdm and self.verbose:
+                print(f'running prediction: {len(slicers)} steps')
+
+            # BATCHED INFERENCE LOOP
+            with tqdm(desc=None, total=len(slicers), disable=not self.allow_tqdm) as pbar:
+                for i in range(0, len(slicers), self.batch_size):
+                    batch_slicers = slicers[i:i + self.batch_size]
+                    
+                    # Create batch
+                    # if data is on CPU, this moves slices to GPU. 
+                    # If data is on GPU, this stays on GPU.
+                    batch = torch.stack([data[s] for s in batch_slicers]).to(self.device)
+
+                    # Predict
+                    # We pass the batch to internal_maybe_mirror_and_predict. 
+                    # nnU-Net handles batches correctly in this method (flipping the whole batch).
+                    with torch.autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+                        # [0] because it returns a tuple, usually (prediction, None)
+                        prediction = self._internal_maybe_mirror_and_predict(batch)[0]
+                    
+                    # Move prediction to result device (if different from GPU)
+                    prediction = prediction.to(results_device)
+
+                    # Accumulate results for the batch
+                    for j, sl in enumerate(batch_slicers):
+                        # Apply Gaussian
+                        pred_patch = prediction[j]
+                        if self.use_gaussian:
+                            pred_patch *= self.gaussian
+                            
+                        predicted_logits[sl] += pred_patch
+                        n_predictions[sl[1:]] += self.gaussian
+
+                    pbar.update(len(batch_slicers))
+
+            # predicted_logits /= n_predictions
+            torch.div(predicted_logits, n_predictions, out=predicted_logits)
+            
+            # check for infs
+            if torch.any(torch.isinf(predicted_logits)):
+                raise RuntimeError('Encountered inf in predicted array. Aborting... If this problem persists, '
+                                   'reduce value_scaling_factor in compute_gaussian or increase the dtype of '
+                                   'predicted_logits to fp32')
+        except Exception as e:
+            del predicted_logits, n_predictions, prediction, gaussian, batch
             empty_cache(self.device)
             empty_cache(results_device)
             raise e
